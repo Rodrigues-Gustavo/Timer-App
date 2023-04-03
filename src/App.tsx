5c23860be511299dc1119860bff65c877a50cf61
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-04-03 11:55:51 -0300

    config project design

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { defaultTheme } from './styles/theme/default'
+import { GlobalStyle } from './styles/globalStyle'
+import { Router } from './Router'
+
 const App = () => {
   return (
-    <div>App</div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from './styles'
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <span>LOGO</span>
+      <nav>
+        <a href="">Timer</a>
+        <a href="">History</a>
+      </nav>
+    </HeaderContainer>
+  )
+}
