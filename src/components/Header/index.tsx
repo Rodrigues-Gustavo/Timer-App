c0059c73be7f4fb8ba3eeb732bae186a590baa72
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-04-04 12:36:35 -0300

    creating header and home page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
+import { Scroll, Timer } from 'phosphor-react'
 import { HeaderContainer } from './styles'
+import { NavLink } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <HeaderContainer>
+      {/* <img src={logo} alt="" /> */}
       <span>LOGO</span>
       <nav>
-        <a href="">Timer</a>
-        <a href="">History</a>
+        <NavLink to="/" title="timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="histórico">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,33 @@
+import { Play } from 'phosphor-react'
+
+import { HomeContainer, StartCountdownButton } from './styles'
+
+export const Home = () => {
+  return (
+    <HomeContainer>
+      <form action="">
+        <div>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+          <span>minutos.</span>
+        </div>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
